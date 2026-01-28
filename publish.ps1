d3ad2c90b6530de5ece1a85f37667dd54b792e1a
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2026-01-28 00:05:56 +0000

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -3,4 +3,6 @@ $env:GNUPGHOME = "C:\Users\adm101\AppData\Roaming\gnupg"
 
 $KEY="59609C262707C907AFABFF9D5F24E05A6AFEDD4C"
 
-& mvn -f .\client\pom.xml -B -Psign-artifacts -DskipTests "-Dgpg.keyname=$KEY" "-Dgpg.executable=$env:GPGEXE" "-Dgpg.homedir=$env:GNUPGHOME" -e clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
+& mvn -f .\client\pom.xml -Psign-artifacts -DskipTests "-Dgpg.keyname=$KEY" "-Dgpg.executable=$env:GPGEXE" "-Dgpg.homedir=$env:GNUPGHOME" -e clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
+
+#& mvn -f .\client\pom.xml -B -Psign-artifacts -DskipTests "-Dgpg.keyname=$KEY" "-Dgpg.executable=$env:GPGEXE" "-Dgpg.homedir=$env:GNUPGHOME" -e clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
